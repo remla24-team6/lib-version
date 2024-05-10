c3dfd9805099d1daf3c4003263f95df8da3085c2
Adit Whorra <aditwhorra@gmail.com>
2024-05-10 12:58:45 +0200

    Changed library name

--- versioning_remla/versioning.py
+++ versioning_remla/versioning.py
@@ -1,16 +1,13 @@
+""" Utlility class to make this package version-aware"""
 from importlib_resources import files
 
 
-class VersioningUtil:
-    def get_version(self):
+class VersionUtil:
+    """Utlility class to make this package version-aware"""
+    def get_version(self) -> str:
         """This library is version-aware. This method fetches the current version from the project metadata and returns it.
-        Returns the version of this library.
-        """ 
+        Returns:
+            str: Returns the version of this library.
+        """
         version = files('resources').joinpath('version.txt').read_text()
         return version
-    
-    
-if __name__ == "__main__":
-    util = VersioningUtil()
-    
-    print(util.get_version())
